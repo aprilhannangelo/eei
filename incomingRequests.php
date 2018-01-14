8001c90c4da5d5bbc7a8da66d6619896ffad07c8
April Angelo <aprilangelo@yahoo.com>
2018-01-14 23:55:21 +0800

    JAN14_Updates - Merged Donna's files

--- incomingRequests.php
+++ incomingRequests.php
@@ -628,14 +628,23 @@
             </div>
           </form><!-- End of User Access Request Form -->
 
-          <!-- ************** IMPORT JAVASCRIPT ************* -->
+
+          <!-- IMPORT JS SCRIPTS HERE -->
+          <!--Import jQuery before materialize.js. Do not change arrangement -->
+
           <!--JQuery version of Materialize-->
           <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
           <!--Import materialize.js-->
           <script type="text/javascript" src="js/materialize.min.js"></script>
-          <!--Import javascript.js for DataTables script-->
+
+          <!-- Import javascript for DataTables  -->
+          <script src='https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js'></script>
           <script type="text/javascript" src="js/javascript.js"></script>
+
           <!--Import for Sweet Alert-->
           <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+          <!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script> -->
     </body>
 </html>
